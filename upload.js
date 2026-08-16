7787c4a7b490964848d4a21b9f8f021093babbe0
chalieyigzaw78-stack <chalieyigzaw78@gmail.com>
2026-08-16 11:44:48 +0300

    Update upload.js

--- upload.js
+++ upload.js
@@ -19,7 +19,7 @@ const storage = new CloudinaryStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB max
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 module.exports = { upload, cloudinary };
